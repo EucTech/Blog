9183323b733894d6c101207442eea482b8fc8df6
Euccreative <euccreative@gmail.com>
2022-10-24 02:38:32 +0100

    work

--- js/main.js
+++ js/main.js
@@ -71,3 +71,28 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+
+const navlink = document.querySelector('.navlinks')
+const navlinka = document.querySelectorAll('.navlinks a')
+const navOpen = document.querySelector('.open')
+const navClose = document.querySelector('.close')
+
+
+function navopen(){
+    navlink.style.top ="0%";
+    navOpen.style.display = "none"
+    navClose.style.display = "block"
+}
+
+function navclose(){
+    navlink.style.top ="-120%";
+    navOpen.style.display = "block"
+    navClose.style.display = "none"
+}
+
+navlinka .forEach(navLink => {
+    navLink.addEventListener('click', function(){
+        navclose()
+    })
+  })
